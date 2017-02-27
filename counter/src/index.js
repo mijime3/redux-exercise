65f91ca984e7cc992f0d4fe4a90a5b86b1cd52e9
Hajime IGARI <orkafriday@gmail.com>
2017-02-27 23:00:51 +0900

    update readme, add comment

--- counter/src/index.js
+++ counter/src/index.js
@@ -4,16 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // 'redux'モジュールから `createStore という名前で export された値` を'createStore'という名前で読み込む
 import { createStore } from 'redux';
+// プロジェクト固有のモジュールも読み込める、ここでは相対パスでファイルパス（拡張子は不要）を指定している
 import Counter from './components/Counter';
+// ディレクトリパスだけを指定した場合は index.js が読み込まれる
 import counter from './reducers';
 
-// JSはステートメント末尾にセミコロンがなくても動作する（ASIという機能で、自動的に補完される）
+// * JSはステートメント末尾にセミコロンがなくても動作する（ASIという機能で、自動的に補完される）
 const store = createStore(counter)
 const rootEl = document.getElementById('root');
 
-// `() =>` は arrow function
-// `()` には引数を記述する
-// `=>` の後は関数の本体
+// * `() =>` は arrow function  `()` には引数を記述する   `=>` の後は関数の本体
+// `ReactDOM.render()` には、JSXとcontainer(DOM node)を渡す
 const render = () => ReactDOM.render(
   <Counter
     value={store.getState()}
