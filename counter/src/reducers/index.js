36f239e36ea3998d738bc11c52a9273ac51220ff
Hajime IGARI <igari@mkt-apps.com>
2017-02-28 13:45:00 +0900

    add comment

--- counter/src/reducers/index.js
+++ counter/src/reducers/index.js
@@ -1,9 +1,12 @@
+// action と現在の state を受け取り、**新しい** state を返す
+// 受け取った state を変更してはいけない 
 export default (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return state + 1;
     case 'DECREMENT':
       return state - 1;
+    // action が不明の場合は state をそのまま返す
     default:
       return state
   }
