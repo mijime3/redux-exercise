db7ae02fdc05084f1e2686105fbf129238113684
Hajime IGARI <igari@mkt-apps.com>
2017-03-18 16:58:18 +0900

    comment

--- todos/src/reducers/index.js
+++ todos/src/reducers/index.js
@@ -2,6 +2,14 @@ import { combineReducers } from 'redux';
 import todos from './todos';
 import visibilityFilter from './visibilityFilter';
 
+// `combineReducers()` は複数の reducer を１つにまとめる
+// state は 各 reducer 名をキーとしたツリー構造のデータとなる
+// 以下の場合は次のような state になる
+// state = {
+//    "todos": [todo1, todo2, ...],
+//    "visibilityFilter": "SHOW_ALL"
+// }
+// 各 reducer で扱う state は、自身の名前のキーに対する値の部分だけになる
 const todoApp = combineReducers({
   todos,
   visibilityFilter
